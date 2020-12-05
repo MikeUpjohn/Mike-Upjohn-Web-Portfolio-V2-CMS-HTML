b2e5f64d17e49f18718a2a2ab8c0851f4e7981ab
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2020-12-05 19:15:42 +0000

    Map editor page WIP.

---
+++ js/map.js
@@ -0,0 +1,31 @@
+var defaultFadeIn = 500;
+
+$(document).ready(function() {
+	if($("#route-map-area").length > 0) {
+		$("#route-map-area").height($(window).height());
+		$("#route-map").height($(window).height() - $(".close-map").height() - 20);
+
+		$("#open-map-editor").click(function(e) {
+			e.preventDefault();
+			// initialiseMap();
+			openMap();
+		});
+
+		$("#route-map-close").click(function(e) {
+			e.preventDefault();
+			closeMap();
+		});
+	}
+});
+
+function openMap() {
+	$("#route-map-area").fadeIn(defaultFadeIn);
+}
+
+function closeMap() {
+	$("#route-map-area").fadeOut(defaultFadeIn);
+}
+
+function initialiseMap() {
+	
+}
