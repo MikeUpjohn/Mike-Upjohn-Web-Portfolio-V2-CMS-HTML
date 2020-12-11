92af8b59ee7f6b22982cb4b5522ac0c6cfd9f8b4
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2020-12-11 09:47:25 +0000

    Route waypoints added.

--- js/map.js
+++ js/map.js
@@ -3,8 +3,13 @@ let closeButtonHeight = 82;
 
 $(document).ready(function() {
 	if($("#route-map-area").length > 0) {
+		var mapHeight = $(window).height() - $(".close-map").height() - 20;
+
 		$("#route-map-area").height($(window).height());
-		$("#route-map").height($(window).height() - $(".close-map").height() - 20);
+		$("#route-map").height(mapHeight);
+		$("#map-area").height(mapHeight);
+		$("#route-waypoints").height(mapHeight / 2);
+		$("#route-markers").height(mapHeight / 2);
 
 		$("#open-map-editor").click(function(e) {
 			e.preventDefault();
@@ -15,6 +20,20 @@ $(document).ready(function() {
 		$("#route-map-close").click(function(e) {
 			e.preventDefault();
 			closeMap();
+		});
+
+		$("#route-waypoint-add").click(function(e) {
+			e.preventDefault();
+			console.log("clicked add route waypoint");
+		});
+
+		$(".route-waypoints-list li .delete").click(function(e) {
+			e.preventDefault();
+			console.log("clicked delete");
+		});
+
+		$(".route-waypoints-list").sortable({
+			handle: ".drag-handle"
 		});
 	}
 });
