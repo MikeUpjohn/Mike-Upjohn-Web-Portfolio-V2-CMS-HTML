9e5d23df86b4bb63f46155220c65d7d1fad2ec21
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2020-12-19 10:01:54 +0000

    Added scrollbars to waypoints and markers panels.

--- js/map.js
+++ js/map.js
@@ -3,13 +3,35 @@ let closeButtonHeight = 82;
 
 $(document).ready(function() {
 	if($("#route-map-area").length > 0) {
+		
+		// Set these up first, then control height after scrollbars created
+		if($("#route-waypoints").length > 0) {
+			$("#route-waypoints").mCustomScrollbar({
+				axis: "y",
+				theme: "dark",
+				setHeight: true
+			});
+		}
+
+		if($("#route-markers").length > 0) {
+			$("#route-markers").mCustomScrollbar({
+				axis: "y",
+				theme: "dark",
+				setHeight: true
+			});
+		}
+
+
 		var mapHeight = $(window).height() - 216; // Arbitrary number to account for the header, footer and padding.
 
 		$("#route-map-area").height($(window).height());
 		$("#route-map").height(mapHeight);
 		$("#map-area").height(mapHeight);
-		$("#route-waypoints").height(mapHeight / 2);
-		$("#route-markers").height(mapHeight / 2);
+		/*$("#route-waypoints").height(mapHeight / 2);
+		$("#route-markers").height(mapHeight / 2);*/
+		$("#route-waypoints").css("height", mapHeight / 2 + "px");
+		$("#route-markers").css("height", mapHeight / 2 + "px");
+
 
 		$("#open-map-editor").click(function(e) {
 			e.preventDefault();
